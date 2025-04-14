f56eff7988375be24b9716527a1517cfd7ac59e5
FrancoEsterellasUA <festerellas@mail.austral.edu.ar>
2025-04-14 10:56:20 -0300

    Update line.py

--- line.py
+++ line.py
@@ -16,6 +16,6 @@ def line():
     print('P1', p1)
     print('P2', p2)
     distancia= ((p2[0]-p1[0])**2+(p2[1]-p1[1])**2)*0.5
-    print('La distancia entre ellos es:' distancia)
+    print('La distancia entre ellos es:', distancia)
 
 
